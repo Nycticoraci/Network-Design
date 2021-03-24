63804098d3b74937638962cf2a4e1d6dace15a4c
Nycticoraci <37623397+Nycticoraci@users.noreply.github.com>
2021-03-24 18:59:03 -0400

    Update phase3_client.py

--- phase3_client.py
+++ phase3_client.py
@@ -1,57 +1,150 @@
-from socket import *
-import time
 import os
-import subprocess
 import math
-import tkinter as tk
-from tkinter import simpledialog
+import array
+import random
+import datetime
+import time
+from socket import *
 
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+HOST = 'localhost'
+PORT = 12000
+client_socket = socket(AF_INET, SOCK_DGRAM)
 
-# Creates simple popup GUI
-root = tk.Tk()
-root.withdraw()
-fileName = simpledialog.askstring(title = 'Client Input',
-                                  prompt = 'Hello friend! What is the path for your file?')
 
-# Creates a list where each index holds 1024 bytes of the image file
-# in sequential order
-def Make_Packet(packet, data):
+# Opens the file, reads it in bytes, and puts 1024 bit chunks into a list
+def read_file(data, size):
     packet_list = []
-    userFile = open(data, 'rb')
-    packet_size = userFile.read(1024)
-    for i in range(packet):
-        packet_list.append(packet_size)
-        packet_size = userFile.read(1024)
+    user_file = open(data, 'rb')
+    payload = user_file.read(1024)
+    for i in range(size):
+        packet_list.append(payload)
+        payload = user_file.read(1024)
     return packet_list
 
-outputName = fileName
-linux = subprocess.run(['uname'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#print(linux.returncode)
-if linux.returncode == 0:
-    i = outputName.rfind('/')                               #If uname completes successfully (return code 0), linux env is true
-else:                                                       #else run path for windows
-    i = outputName.rfind('\\')                              #Get the last path index
-#print(i)                                                   #Check the index of the last path (file name)
-print('File name is: '+ outputName[i+1:])                   #Verify user input is correct
-print('File path is: '+ fileName)
-outputName = outputName[i+1:].encode('utf8')                #Encode file name
-clientSocket.sendto(outputName,(serverName, serverPort))    #Send file name to server
-time.sleep(0.02)                             # Pause the process so that the server does not mix up the packets for file name and fize size
-file_size = os.stat(fileName).st_size        # Gets size of the file in bytes
-NR = str(math.ceil(file_size / 1024))        # Returns ceiling of filesize divided by 1024
-print('Number of Receives is ' + NR)         # Prints number of packets to send
-number_receives = NR.encode('utf8')
-clientSocket.sendto(number_receives,(serverName, serverPort))    # Sends number to server
 
-# Sends Make_Packet() the # of divisions to make, as well
-# as the file to divide
-rdt_send = Make_Packet(int(NR), fileName)
+# Combines the sequence, data, and checksum combined
+def make_pkt(seq, data, checksum):
+    return seq + data + checksum
 
-# Sends each 1024 byte division of the image file in order
-for packet in rdt_send:
-    clientSocket.sendto(packet,(serverName, serverPort))
-    time.sleep(0.02)
-clientSocket.close()
+
+# Computes the checksum from the data
+def make_checksum(data):
+    if len(data) % 2 != 0:
+        data += b'\0'
+    byte = sum(array.array('H', data))
+    byte = (byte >> 16) + (byte & 0xffff)
+    byte += byte >> 16
+    checksum = (~byte) & 0xffff
+    checksum = str(checksum).encode()
+    return checksum
+
+
+# Returns TRUE if packet is received, else FALSE
+def rdt_rcv(rcvpkt):
+    if rcvpkt:
+        return True
+    else:
+        return False
+
+
+# Returns TRUE if the server checksum matches the sent one, else FALSE
+def corrupt(rcvpkt, checksum):
+    checksum_recalc = rcvpkt[2:]
+    if checksum == checksum_recalc:
+        return False
+    else:
+        return True
+
+
+# Sends the packet
+def udt_send(sndpkt, HOST = 'localhost', PORT = 12000):
+    client_socket.sendto(sndpkt, (HOST, PORT))
+    return
+
+
+# Returns TRUE if the ACk macthes the expected value, else FALSE
+def isACK(rcvpkt, check):
+    ACK = rcvpkt[:1]
+    if ACK == check:
+        return True
+    else:
+        return False
+
+
+# File name requested, then divided into 1024 bit chunks + remainder
+file_name = input('Enter filename: ')
+option    = input('Selection option [1/2/3]: ')
+if option == '2':
+    option = b'2'
+elif option == '3':
+    option = b'3'
+else:
+    option = b'1'
+file_size = os.stat(file_name).st_size
+number_of_receives = math.ceil(file_size / 1024)
+
+# Program starts at "call 0 from above"
+call = 0
+
+#print(datetime.datetime.now())
+tic = time.perf_counter()
+rdt_send = read_file(file_name, number_of_receives)
+udt_send(option)
+
+# "Call from above"
+for data in rdt_send:
+    # If this is a call 0
+    if call == 0:
+        # Calcuate the checksum, order it sequence 0, and send w/ the data
+        checksum = make_checksum(data)
+        sndpkt = make_pkt(b'0', data, checksum)
+        udt_send(sndpkt)
+
+        # If the packet is received but data or ACK is corrupt, repeat.
+        # When none of them are corrupt, call will change to 1 and move on
+        client_socket.settimeout(0.1)
+        try:
+            rcvpkt, addr = client_socket.recvfrom(1024)
+        except:
+            break
+
+        while rdt_rcv(rcvpkt) is True and (corrupt(rcvpkt, checksum) is True or isACK(rcvpkt, b'1') is True):
+            udt_send(sndpkt)
+            client_socket.settimeout(0.1)
+            try:
+                rcvpkt, addr = client_socket.recvfrom(1024)
+            except:
+                break
+                rcvpkt, addr = client_socket.recvfrom(1024)
+
+        # Next call will be 1
+        call = 1
+
+    # If this is a call 1
+    elif call == 1:
+        # Calcuate the checksum, order it sequence 1, and send w/ the data
+        checksum = make_checksum(data)
+        sndpkt = make_pkt(b'1', data, checksum)
+        udt_send(sndpkt)
+
+        # If the packet is received but data or ACK is corrupt, repeat.
+        # When none of them are corrupt, call will change to 1 and move on
+        client_socket.settimeout(0.1)
+        try:
+            rcvpkt, addr = client_socket.recvfrom(1024)
+        except:
+            break
+
+        while rdt_rcv(rcvpkt) is True and (corrupt(rcvpkt, checksum) is True or isACK(rcvpkt, b'0') is True):
+            udt_send(sndpkt)
+            client_socket.settimeout(0.1)
+            try:
+                rcvpkt, addr = client_socket.recvfrom(1024)
+            except:
+                break
+
+        # Next call will be 0
+        call = 0
+toc = time.perf_counter()
+print('Done.')
+print(f'Time Elapsed: {toc-tic:0.4f}')
