d94d83d162b3fdadbc2125991ee60872535756de
Flood232 <70727852+Flood232@users.noreply.github.com>
2021-04-07 17:10:51 -0400

    Add files via upload

---
+++ JackP5Client.py
@@ -0,0 +1,200 @@
+import os
+import math
+import array
+import random
+import datetime
+import time
+from socket import *
+
+HOST = 'localhost'
+PORT = 12000
+client_socket = socket(AF_INET, SOCK_DGRAM)
+
+
+# Opens the file, reads it in bytes, and puts 1024 bit chunks into a list
+def read_file(data, size):
+    packet_list = []
+    user_file = open(data, 'rb')
+    payload = user_file.read(1024)
+    for i in range(size):
+        packet_list.append(payload)
+        payload = user_file.read(1024)
+    return packet_list
+
+
+# Combines the sequence, data, and checksum combined
+def make_pkt(seq, data, checksum):
+    return seq + data + checksum
+
+
+# Computes the checksum from the data
+def make_checksum(data):
+    if len(data) % 2 != 0:
+        data += b'\0'
+    byte = sum(array.array('H', data))
+    byte = (byte >> 16) + (byte & 0xffff)
+    byte += byte >> 16
+    checksum = (~byte) & 0xffff
+    checksum = str(checksum).encode()
+    return checksum
+
+
+# Returns TRUE if packet is received, else FALSE
+def rdt_rcv(rcvpkt):
+    if rcvpkt:
+        return True
+    else:
+        return False
+
+
+# Returns TRUE if the server checksum matches the sent one, else FALSE
+def corrupt(rcvpkt, checksum):
+    checksum_recalc = rcvpkt[2:]
+    if checksum == checksum_recalc:
+        return False
+    else:
+        print("Expected Checksum: ", checksum)
+        print("Received Checksum: ", checksum_recalc)
+        return True
+
+
+# Sends the packet
+def udt_send(sndpkt, HOST = 'localhost', PORT = 12000):
+    client_socket.sendto(sndpkt, (HOST, PORT))
+    return
+
+
+# Returns TRUE if the ACk macthes the expected value, else FALSE
+def isACK(rcvpkt, check):
+    ACK = rcvpkt[:1]
+    if ACK == check:
+        return True
+    else:
+        return False
+
+def ACK_error (rcvpkt, bitVal, loss_rate):
+    err_chance = random.randint(1, 101)
+    if err_chance < loss_rate:
+        rcvpkt[:1] = bitVal
+    return rcvpkt
+
+# Simulates ACK loss by replacing ACK with the previous ACK
+def ACK_loss(ack, ackPrev, loss_rate):
+    err_chance = random.randint(1, 101)
+    if err_chance < loss_rate:
+        ack = ackPrev
+    return ack
+
+def seqValBtyes(seqVal):
+    if (seqVal == 0):
+        return b'0'
+    elif (seqVal == 1):
+        return b'1'
+    elif (seqVal == 2):
+        return b'2'
+    elif (seqVal == 3):
+        return b'3'
+    elif (seqVal == 4):
+        return b'4'
+    elif (seqVal == 5):
+        return b'5'
+    elif (seqVal == 6):
+        return b'6'
+    elif (seqVal == 7):
+        return b'7'
+    elif (seqVal == 8):
+        return b'8'
+    elif (seqVal == 9):
+        return b'9'
+    else:
+        print("Sequence Error")
+
+def refuse_data(data):
+    # DO NOTHING
+    data = data
+
+def getAckNum(rcvpkt):
+    ACK = rcvpkt[:1]
+    return ACK
+
+# File name requested, then divided into 1024 bit chunks + remainder
+file_name = input('Enter filename: ')
+option    = input('Selection option [ 1: No Error/ 2: ACK Error/ 3: Data Error/ 4: ACK Loss/ 5. Data Loss]: ')
+if option == '2':
+    option = b'2'
+elif option == '3':
+    option = b'3'
+elif option == '4':
+    option = b'4'
+elif option == '5':
+    option = b'5'
+else:
+    option = b'1'
+
+if option == b'4':
+    ACK_ls  = 21
+else:
+    ACK_ls  = 0
+
+if option == b'2':
+    ACK_err  = 61
+else:
+    ACK_err = 0
+
+file_size = os.stat(file_name).st_size
+number_of_receives = math.ceil(file_size / 1024)
+
+# Program starts at "call 0 from above"
+call = 0
+
+print(datetime.datetime.now())
+rdt_send = read_file(file_name, number_of_receives)
+udt_send(option)
+
+prevpkt = 0
+
+base = 1
+nextSeqNum = 1
+windowSize = 10
+sndpkt = [0] * (number_of_receives + 10)
+
+# "Call from above"
+for data in rdt_send:
+
+    seqDivider = nextSeqNum / 10
+    seqNumMod = nextSeqNum % 10
+    seq = seqValBtyes(seqNumMod)
+    if (nextSeqNum < base + windowSize):
+        checksum = make_checksum(data)
+        print("Seq: ", seq)
+        sndpkt[nextSeqNum] = make_pkt(seq, data, checksum)
+        udt_send(sndpkt[nextSeqNum])
+        if (base == nextSeqNum):
+            startTimer = time.time()
+
+        nextSeqNum += 1
+    else:
+        refuse_data(data)
+    
+    client_socket.settimeout(0.1)
+    try:
+        rcvpkt, addr = client_socket.recvfrom(1024)
+    except:
+        break
+
+    if(rdt_rcv(rcvpkt) is True and corrupt(rcvpkt, checksum) is False):
+        ackBytes = getAckNum(rcvpkt)
+        ackInt = int.from_bytes(ackBytes, "little") - 48
+        base = nextSeqNum + ackInt #+ 1 - 0.1
+        print("Not corrupt! :)")
+        print("Seq: ", seq)
+        print("Base: ", base)
+        #print("Base: ", base)
+        if(base == seq):
+            timerStop = 1
+        else:
+            timerStop = 0
+            startTimer = time.time()
+
+   
+print('Done.')
---
+++ JackP5Server.py
@@ -0,0 +1,214 @@
+import array
+import random
+import datetime
+from socket import *
+
+HOST = 'localhost'
+PORT = 12000
+server_socket = socket(AF_INET, SOCK_DGRAM)
+server_socket.bind((HOST, PORT))
+
+
+# Determines if a packet has been successfully received
+def rdt_rcv(rcvpkt):
+    if rcvpkt:
+        return True
+    else:
+        return False
+
+
+# Compares the checksums and returns TRUE if they are NOT corrupt
+def notcorrupt(rcvpkt):
+    data = rcvpkt[1:][:1024]
+    checksum = rcvpkt[1025:]
+    try:
+        byte = sum(array.array('H', data))
+    except ValueError:
+        print('Done.')
+        print(datetime.datetime.now())
+        exit()
+    byte = (byte >> 16) + (byte & 0xffff)
+    byte += byte >> 16
+    checksum_recalc = (~byte) & 0xffff
+    checksum_recalc = str(checksum_recalc).encode()
+
+    if checksum == checksum_recalc:
+        return True
+    else:
+        return False
+
+
+# Determines if the packet is sequence 0, returns TRUE if so
+def has_seq0(rcvpkt):
+    seq = rcvpkt[:1]
+    if seq == b'0':
+        return True
+    else:
+        return False
+
+
+# Extracts the data from the packet and returns it
+def extract(rcvpkt):
+    return rcvpkt[1:][:1024]
+
+
+# Writes the extracted data to the file
+def deliver_data(data):
+    new_file.write(data)
+    return
+
+
+# Calculates and returns the checksum from the received data
+def checksum(rcvpkt):
+    byte = sum(array.array('H', rcvpkt))
+    byte = (byte >> 16) + (byte & 0xffff)
+    byte += byte >> 16
+    checksum_recalc = (~byte) & 0xffff
+    checksum_recalc = str(checksum_recalc).encode()
+    return checksum_recalc
+
+
+# Makes the packet consisting of the ACK (0 or 1), level (0 or 1), and checksum
+def make_pkt(ACK, seq, checksum):
+    return ACK + b'0' + checksum
+
+
+# Sends the packet at the address
+def udt_send(sndpkt, addr):
+    server_socket.sendto(sndpkt, addr)
+
+
+# Compares the checksums and returns TRUE if they ARE corrupt
+def corrupt(data):
+    data = rcvpkt[1:][:1024]
+    checksum = rcvpkt[1025:]
+    byte = sum(array.array('H', data))
+    byte = (byte >> 16) + (byte & 0xffff)
+    byte += byte >> 16
+    checksum_recalc = (~byte) & 0xffff
+    checksum_recalc = str(checksum_recalc).encode()
+
+    if checksum == checksum_recalc:
+        return False
+    else:
+        return True
+
+
+# Determines if the packet is sequence 1, returns TRUE if so
+def has_seq1(rcvpkt):
+    seq = rcvpkt[:1]
+    if seq == b'1':
+        return True
+    else:
+        return False
+
+
+def DATA_corrupt(data, err_rate):
+    err_chance = random.randint(1, 101)
+    if err_chance < err_rate:
+        l_shift_data = data[0:10]
+        r_shift_data = data[10:]
+        shifted_data = r_shift_data + l_shift_data
+        data = shifted_data
+    return data
+
+
+def ACK_corrupt(ack, err_rate):
+    err_chance = random.randint(1, 101)
+    if err_chance < err_rate:
+        ack = b'2'
+    return ack
+
+def DATA_loss(data, dataPrev, loss_rate):
+    err_chance = random.randint(1, 101)
+    if err_chance < loss_rate:
+        data = dataPrev
+    return data
+
+def seqValBtyes(seqVal):
+    if (seqVal == 0):
+        return b'0'
+    elif (seqVal == 1):
+        return b'1'
+    elif (seqVal == 2):
+        return b'2'
+    elif (seqVal == 3):
+        return b'3'
+    elif (seqVal == 4):
+        return b'4'
+    elif (seqVal == 5):
+        return b'5'
+    elif (seqVal == 6):
+        return b'6'
+    elif (seqVal == 7):
+        return b'7'
+    elif (seqVal == 8):
+        return b'8'
+    elif (seqVal == 9):
+        return b'9'
+    else:
+        print("Sequence Error")
+
+def has_seq(rcvpkt, expectedSeq):
+    seq = rcvpkt[:1]
+    if seq == expectedSeq:
+        return True
+    else:
+        return False
+
+
+print('Waiting...')
+
+# Selects the option
+option, addr = server_socket.recvfrom(2048)
+
+# Option 1: No errors. 2: 20% chance ACk corrupt. 3: 20% chance data corrupt.
+if option == b'1':
+    DATA_err = 0
+elif option == b'2':
+    DATA_err = 0
+elif option == b'3':
+    DATA_err = 21
+else:
+    DATA_err = 0
+
+if option == b'5':
+    DATA_ls = 21
+else:
+    DATA_ls = 0
+
+new_file = open('output.jpg', 'wb')
+
+# This gets the loop started and allows it to continue internally
+oncethru = 0
+prevpkt = 0
+expectedSeqNum = 1
+
+while True:
+    while True:
+        seqNumMod = expectedSeqNum % 10
+        expectedSeq = seqValBtyes(seqNumMod)
+        rcvpkt, addr = server_socket.recvfrom(2048)
+        #info = extract(rcvpkt)
+        #sndpkt = make_pkt(b'0', b'0', checksum(info))
+        #print("Checksum Info: ", checksum(info))
+        '''
+        rcvpkt = DATA_corrupt(rcvpkt, DATA_err)
+        savepkt = rcvpkt
+        rcvpkt = DATA_loss(rcvpkt, prevpkt, DATA_ls)
+        prevpkt = savepkt
+        '''
+        # @: Wait for 0 from below. If these trigger, GOTO "Wait for 1 from below."
+        if rdt_rcv(rcvpkt) is True and notcorrupt(rcvpkt) is True and has_seq(rcvpkt, expectedSeq) is True:
+            print("If is executing")
+            data = extract(rcvpkt)
+            deliver_data(data)
+            ACK = expectedSeq
+            sndpkt = make_pkt(ACK, expectedSeq, checksum(data))
+            udt_send(sndpkt, addr)
+            expectedSeqNum += 1
+            oncethru = 1
+            break
+        else:
+            print("Else is executing",rdt_rcv(rcvpkt),notcorrupt(rcvpkt),has_seq(rcvpkt, expectedSeq) )
+            udt_send(sndpkt, addr)
